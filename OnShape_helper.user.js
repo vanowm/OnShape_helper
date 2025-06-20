23f3b77d9fffbecbe52ea52a3db42ee3fd3aa5f6
V@no <git@vano.org>
2025-06-19 23:03:15 -0400

    ! input field not resized when a variable inserted ! input field shows behind floating buttons/windows + configuration top/bottom rows disabled up/down buttons

--- OnShape_helper.user.js
+++ OnShape_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OnShape helper
 // @namespace    V@no
-// @version      25.6.20
+// @version      25.6.20-030029
 // @description  Various tweaks for OnShape, such as remap F2 for rename (SHIFT + N)
 // @author       V@no
 // @license      MIT
@@ -12,51 +12,193 @@
 // @grant        none
 // ==/UserScript==
 
+(CSS =>
 {
-	// eslint-disable-next-line no-unused-expressions
-	"use strict";
+// eslint-disable-next-line no-unused-expressions
+"use strict";
 /*
 ^ = CTRL
 ! = ALT
 + = SHIFT
 */
-	const VERSION = "25.6.20";
-	const CHANGES = `+ history resize animation
-+ link to install from greasyfork website`;
-	const map = {
-		"F2": {key: "N", code: "KeyN", keyCode: 78, shiftKey: true}
-	};
+const VERSION = "25.6.20-030029";
+const CHANGES = `! input field not resized when a variable inserted
+! input field shows behind floating buttons/windows
++ configuration top/bottom rows disabled up/down buttons`;
+const map = {
+	"F2": {key: "N", code: "KeyN", keyCode: 78, shiftKey: true}
+};
 
-	let mouseEvent = {};
-	document.addEventListener("mousemove", evt =>
+const elStyle = document.createElement("style");
+elStyle.id = "onShapeHelper";
+elStyle.textContent = CSS;
+document.head.append(elStyle);
+
+let mouseEvent = {};
+document.addEventListener("mousemove", evt =>
+{
+	mouseEvent = evt;
+}, false);
+
+document.body.addEventListener("keydown", evt =>
+{
+	let modifier = "";
+	modifier = evt.altKey ? "!" : "";
+	modifier = evt.shiftKey ? "+" : "";
+	modifier = evt.ctrlKey || evt.metaKey ? "^" : "";
+	const key = modifier + evt.code;
+	if (!evt.isTrusted || !(key in map) || evt.altKey || evt.shiftKey || evt.ctrlKey || evt.metaKey)
+		return;// console.log(evt, mouseEvent);
+
+	if (mouseEvent.target)
 	{
-		mouseEvent = evt;
-	}, false);
+		evt.target.dispatchEvent(new KeyboardEvent(evt.type, Object.assign({}, evt, {key: " ", code: "space", keyCode: 32}, {bubbles: true})));
+		mouseEvent.target.dispatchEvent(new PointerEvent("click", mouseEvent));
+	}
 
-	document.body.addEventListener("keydown", evt =>
+	evt.target.dispatchEvent(new KeyboardEvent(evt.type, Object.assign({}, evt, map[key], {bubbles: true})));
+}, true);
+
+const dataValue = (el, value) =>
+{
+	el.dataset.value = value;
+};
+
+const changeByRegex = /Change by (.+) at (.+)$/;
+
+// eslint-disable-next-line no-unused-vars
+const observer = new MutationObserver((mutationList, _observer) =>
+{
+	const types = {};
+	for (const mutation of mutationList)
 	{
-		let modifier = "";
-		modifier = evt.altKey ? "!" : "";
-		modifier = evt.shiftKey ? "+" : "";
-		modifier = evt.ctrlKey || evt.metaKey ? "^" : "";
-		const key = modifier + evt.code;
-		if (!evt.isTrusted || !(key in map) || evt.altKey || evt.shiftKey || evt.ctrlKey || evt.metaKey)
-			return console.log(evt, mouseEvent);
-
-		if (mouseEvent.target)
+		for(const node of mutation.addedNodes)
 		{
-			evt.target.dispatchEvent(new KeyboardEvent(evt.type, Object.assign({}, evt, {key: " ", code: "space", keyCode: 32}, {bubbles: true})));
-			mouseEvent.target.dispatchEvent(new PointerEvent("click", mouseEvent));
-		}
+			if (node.nodeType !== 1)
+				continue;
 
-		evt.target.dispatchEvent(new KeyboardEvent(evt.type, Object.assign({}, evt, map[key], {bubbles: true})));
-	}, true);
-	const css = `
+			if (node.matches("input:not(.OSH)"))
+			{
+				node.classList.add("OSH");
+				node.parentElement.classList.add("OSH");
+				const eventHandler = () => dataValue(node.parentElement, node.value);
+				node.addEventListener("input", eventHandler);
+				// inserted variables don't trigger input event, so we need to check for changes
+				let previousValue = null;
+				const loop = timestamp =>
+				{
+					if (previousValue !== node.value)
+					{
+						previousValue = node.value;
+						eventHandler();
+					}
+					if (node.isConnected)
+						return requestAnimationFrame(loop);
+				};
+				requestAnimationFrame(loop);
+			}
+			if (node.matches(".os-flex-table-row.change:not(.OSH)"))
+			{
+				node.classList.add("OSH");
+				const changeBy = node.dataset.bsOriginalTitle.match(changeByRegex);
+				const elModified = node.querySelector(".os-flex-col.os-item-modified-date.inside-document");
+				elModified.textContent = `${changeBy[2]}`;
+			}
+			if (!node.classList.contains("OSH_conf"))
+			{
+				const nlNodes = node.querySelectorAll(`a:not(.OSH_conf)[ng-click="configurationTable.moveParameterUp()"], a:not(.OSH_conf)[ng-click="configurationTable.moveParameterDown()"`);
+				if (nlNodes.length === 0)
+					continue;
+
+				types.configuration = nlNodes.length;
+				node.classList.add("OSH_conf");
+				for(let i = 0; i < nlNodes.length; i++)
+				{
+					const elA = nlNodes[i];
+					elA.classList.add("OSH_conf");
+					const elParent = elA.closest("div.os-table-header-responsive-last-row>div.d-flex");
+					elParent.classList.add("OSH_conf_row");
+					if (elParent.upDown === undefined)
+						elParent.upDown = {};
+
+					const type = elA.matches(`[ng-click="configurationTable.moveParameterUp()"]`);
+					if (elParent.upDown[type])
+						elParent.upDown[type].replaceWith(elA);
+					else
+						elParent.prepend(elA);
+
+					elParent.upDown[type] = elA;
+					elA.classList.add(type ? "UP" : "DOWN");
+					elA.title = elA.textContent;
+					elA.textContent = type ? "▲" : "▼";
+
+					elA.addEventListener("click", () => moved(elA.parentElement.parentElement.parentElement));
+				}
+			}
+		}
+	}
+	if (types.configuration)
+	{
+		if (!document.querySelector("div.single-table-container.os-virtual-scroll-section:first-child .UP"))
+		{
+			const elRow = document.querySelector("div.single-table-container.os-virtual-scroll-section:first-child div.OSH_conf_row");
+			const elA = elRow.firstChild.cloneNode(true);
+			elA.setAttribute("ng-click", "configurationTable.moveParameterUp()");
+			elA.classList.remove("DOWN");
+			elA.title = "Move UP";
+			elA.classList.add("OSH_conf", "UP");
+			elA.textContent = "▲";
+			elRow.prepend(elA);
+			const elParent = elA.closest("div.os-table-header-responsive-last-row>div.d-flex");
+			elParent.upDown[true] = elA;
+
+		}
+		if (!document.querySelector("div.single-table-container.os-virtual-scroll-section:last-child .DOWN"))
+		{
+			const elRow = document.querySelector("div.single-table-container.os-virtual-scroll-section:last-child div.OSH_conf_row");
+			const elA = elRow.firstChild.cloneNode(true);
+			elA.setAttribute("ng-click", "configurationTable.moveParameterDown()");
+			elA.classList.remove("UP");
+			elA.title = "Move DOWN";
+			elA.classList.add("OSH_conf", "DOWN");
+			elA.textContent = "▼";
+			elRow.append(elA);
+			const elParent = elA.closest("div.os-table-header-responsive-last-row>div.d-flex");
+			elParent.upDown[false] = elA;
+		}
+	}
+
+});
+observer.observe(document.body, {
+	childList: true,
+	subtree: true,
+});
+
+const moved = el =>
+{
+	moved.clear();
+	el.classList.add("moved");
+	moved.el = el;
+	moved.timer = setTimeout(moved.clear, 2000);
+
+};
+moved.clear = () =>
+{
+	clearTimeout(moved.timer);
+	if (moved.el)
+	{
+		moved.el.classList.remove("moved");
+		moved.el = null;
+	}
+};
+console.log(`OnShape helper v${VERSION} loaded`, "https://greasyfork.org/en/scripts/522636");
+})(`
 .dimension-edit-container .ns-feature-parameter .bti-numeric-text,
 .dimension-edit-container os-quantity-parameter input,
 .dimension-edit
 {
 	max-width: unset;
+	z-index: 9999;
 }
 
 div.OSH::before,
@@ -155,147 +297,4 @@ os-message-bubble .os-message-bubble-container.document-message-bubble {
 	-webkit-filter: brightness(1) !important;
 	filter: brightness(1) !important;
 }
-`;
-	const elStyle = document.createElement("style");
-	elStyle.id = "onShapeHelper";
-	elStyle.textContent = css;
-	document.head.append(elStyle);
-	// const configKeys = [
-	// 	"disableImprinting"
-	// ];
-	// const configValues = {};
-	// const storedValues = (() =>
-	// {
-	// 	try
-	// 	{
-	// 		return JSON.parse(localStorage.getItem(`OSH_configValues`)) || {};
-	// 	}
-	// 	catch
-	// 	{
-	// 		return {};
-	// 	}
-	// })();
-	// for(let i = 0; i < configKeys.length; i++)
-	// {
-	// 	const key = configKeys[i];
-	// 	configValues[key] = key in storedValues ? storedValues[key] : null;
-	// }
-	// let timeoutValues;
-	// const saveValues = () => localStorage.setItem(`OSH_configValues`, JSON.stringify(configValues));
-	const dataValue = (el, value) =>
-	{
-		el.dataset.value = value;
-		// if (el.dataset.parameterId in configValues)
-		// {
-		// 	configValues[el.dataset.parameterId] = value;
-		// 	clearTimeout(timeoutValues);
-		// 	timeoutValues = setTimeout(saveValues, 1000);
-		// }
-		// console.log("input", el, value, el.dataset.parameterId);
-	};
-	// console.log("configValues", configValues);
-	const changeByRegex = /Change by (.+) at (.+)$/;
-	// eslint-disable-next-line no-unused-vars
-	const observer = new MutationObserver((mutationList, _observer) =>
-	{
-		for (const mutation of mutationList)
-		{
-			for(const node of mutation.addedNodes)
-			{
-				if (node.nodeType !== 1)
-					continue;
-
-				if (node.matches("input:not(.OSH)"))
-				{
-					node.classList.add("OSH");
-					node.parentElement.classList.add("OSH");
-					dataValue(node.parentElement, node.value);//node.dataset.bsOriginalTitle);
-					node.addEventListener("input", evt => dataValue(evt.target.parentElement, evt.target.value));
-				}
-				// if (node.matches("os-boolean-parameter:not(.OSH"))
-				// {
-				// 	node.classList.add("OSH");
-				// 	const elInput = node.querySelector("input[type=checkbox]");
-				// 	const elWrapper = node.querySelector("[data-parameter-id]");
-				// 	const id = elWrapper.dataset.parameterId;
-				// 	console.log("os-boolean-parameter", {node, elInput, id, configValues: configValues[id], checked: elInput.checked});
-				// 	if (configValues[id] !== undefined && configValues[id] !== null)
-				// 	{
-				// 		// elWrapper.value = configValues[id];
-				// 		// elInput.dispatchEvent(new Event("click", { bubbles: true,  }));
-				// 		// elInput.click();
-				// 		// elInput.checked = configValues[id];
-				// 		elInput.dispatchEvent(new Event("change", { bubbles: true }));
-				// 		elInput.dispatchEvent(new Event("input", { bubbles: true }));
-				// 		elInput.dispatchEvent(new Event("click", { bubbles: true }));
-
-				// 		console.log("set value", elWrapper.value, id);
-				// 	}
-				// 	// dataValue(elWrapper, elInput.checked);
-				// 	node.addEventListener("input", evt => dataValue(elWrapper, evt.target.checked));
-				// }
-				// console.log(node);
-				if (node.matches(".os-flex-table-row.change:not(.OSH)"))
-				{
-					node.classList.add("OSH");
-					const changeBy = node.dataset.bsOriginalTitle.match(changeByRegex);
-					const elModified = node.querySelector(".os-flex-col.os-item-modified-date.inside-document");
-					elModified.textContent = `${changeBy[2]}`;
-				}
-				if (!node.classList.contains("OSH_conf"))
-				{
-					const nlNodes = node.querySelectorAll(`a:not(.OSH_conf)[ng-click="configurationTable.moveParameterUp()"], a:not(.OSH_conf)[ng-click="configurationTable.moveParameterDown()"`);
-					if (nlNodes.length === 0)
-						continue;
-
-					node.classList.add("OSH_conf");
-					for(let i = 0; i < nlNodes.length; i++)
-					{
-						const elA = nlNodes[i];
-						elA.classList.add("OSH_conf");
-						const elParent = elA.closest("div.os-table-header-responsive-last-row>div.d-flex");
-						elParent.classList.add("OSH_conf_row");
-						if (elParent.upDown === undefined)
-							elParent.upDown = {};
-
-						const type = elA.matches(`[ng-click="configurationTable.moveParameterUp()"]`);
-						if (elParent.upDown[type])
-							elParent.upDown[type].replaceWith(elA);
-						else
-							elParent.prepend(elA);
-
-						elParent.upDown[type] = elA;
-						elA.classList.add(type ? "UP" : "DOWN");
-						elA.title = elA.textContent;
-						elA.textContent = type ? "▲" : "▼";
-
-						elA.addEventListener("click", () => moved(elA.parentElement.parentElement.parentElement));
-					}
-				}
-			}
-		}
-	});
-	observer.observe(document.body, {
-		childList: true,
-		subtree: true,
-	});
-
-	const moved = el =>
-	{
-		moved.clear();
-		el.classList.add("moved");
-		moved.el = el;
-		moved.timer = setTimeout(moved.clear, 2000);
-
-	};
-	moved.clear = () =>
-	{
-		clearTimeout(moved.timer);
-		if (moved.el)
-		{
-			moved.el.classList.remove("moved");
-			moved.el = null;
-		}
-	};
-	console.log(`OnShape helper v${VERSION} loaded`, "https://greasyfork.org/en/scripts/522636");
-}
+`);
--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Various tweaks for OnShape, such as remap F2 for rename (SHIFT + N)
 
 Features:
 * F2 to rename features
-* Automatically resize input fields (helpful when entering formulas or long variable names)
+* Automatically resize input fields (helpful when entering formulas or long variable names)<br>![onshape_input_auto_resize](https://github.com/user-attachments/assets/351e22f3-da5c-4708-95d4-8a12870c9c09)
 * Up/Down buttons for configuration items<br>![OnShape_configuration_updown_buttons](https://github.com/user-attachments/assets/7853bb4e-b15d-44df-bc7d-d5e5107aa3f0)
 * Version history properly sized<br>![onshape_history_mod](https://github.com/user-attachments/assets/6de82b4e-0795-49c0-bea1-3de4f0c5d959)
-
+* Move notifications above tool toolbar away from working area<br>![onshape_moved_notification](https://github.com/user-attachments/assets/1ad94592-3e19-4cde-9df4-dde5143c4e3b)
 
 
 [Install](https://greasyfork.org/en/scripts/522636)
